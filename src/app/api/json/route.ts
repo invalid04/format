947331371e18e6f313f563a1062e427707d0735a
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-11 18:23:32 -0500

    determineSchemaType function

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -1,17 +1,34 @@
 import { NextRequest } from "next/server"
 import { z } from "zod"
 
+const determineSchemaType = (schema: any) => {
+    if(!schema.hasOwnProperty("type")) {
+        if(Array.isArray(schema)) {
+            return "array"
+        } else {
+            return typeof schema 
+        }
+    }
+}
+
+const jsonSchemaToZod = (schema: any) => {
+    const type = determineSchemaType(schema)
+}
+
 export const POST = async (req: NextRequest) => {
 
     const body = await req.json()
     
+    // request validation
     const genericSchema = z.object({
         data: z.string(),
         format: z.object({}).passthrough(),
     })
 
-    const res = genericSchema.parse(body)
-    console.log('res', res)
+    const { data, format } = genericSchema.parse(body)
+
+    // create schema for user format
+    const dynamicSchema = jsonSchemaToZod(format)
 
     return new Response("OK")
 }
